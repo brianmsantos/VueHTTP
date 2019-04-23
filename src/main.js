955affe353997f031a5c0ef1ab124821da233bee
Brian Santos <brianmsantos81@gmail.com>
2019-04-23 15:04:53 -0700

    custom resources created

--- src/main.js
+++ src/main.js
@@ -4,7 +4,17 @@ import App from './App.vue'
 
 Vue.use(VueResource);
 
-Vue.http.options.root = 'https://vuejs-http-4c428.firebaseio.com/data.json'
+Vue.http.options.root = 'https://vuejs-http-4c428.firebaseio.com/'
+
+Vue.http.interceptors.push((request, next) => {
+  console.log(request);
+  if (request.method == 'POST'){
+    request.method = 'PUT';
+  }
+  next(response => {
+    response.json = () => { return {messages: response.body} }
+  });
+});
 
 new Vue({
   el: '#app',
